1744ed7917be17faf6524a4dd5ce60714abfb77e
boturon <boturon@gmail.com>
2019-02-09 02:53:21 +0000

    20190208

---
+++ 1308_IntroductionToProgramming/20190208/02_inputs_and_operations.py
@@ -0,0 +1,4 @@
+n1 = float(input("1st number:"))
+n2 = float(input("2nd number:"))
+
+print("%s over %s equals %s!" % (n1, n2, n1/n2))
---
+++ 1308_IntroductionToProgramming/20190208/03_using_length.py
@@ -0,0 +1,2 @@
+words = input("What are you thinking about?")
+print("Your thought has %s characters and %s words!" % (len(words), len(words.split(" "))))
